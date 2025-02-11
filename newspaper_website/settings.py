53a232c179621b7f48637ecb09186aa6f17c6fdd
Nigel <niggschiputura@gmail.com>
2025-02-11 20:44:55 +0200

    password change and reset

--- newspaper_website/settings.py
+++ newspaper_website/settings.py
@@ -138,5 +138,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
+#  
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+# 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
